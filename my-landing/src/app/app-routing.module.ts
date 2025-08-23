732b3d3e14f02d23d9fa2caec7d8b85a522cf708
danieleon08 <dflp2005@gmail.com>
2025-08-22 22:03:41 -0500

    Agrego inicio header

--- my-landing/src/app/app-routing.module.ts
+++ my-landing/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyprofileComponent } from './myprofile/myprofile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Define your routes here
+
+  { path: "", component: MyprofileComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
