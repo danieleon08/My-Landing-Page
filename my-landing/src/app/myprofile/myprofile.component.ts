d8829929073eb1cf85525c893c4f3534c17532a5
danieleon08 <dflp2005@gmail.com>
2025-08-24 22:51:45 -0500

    agrego cambios fondo

--- my-landing/src/app/myprofile/myprofile.component.ts
+++ my-landing/src/app/myprofile/myprofile.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-myprofile',
   templateUrl: './myprofile.component.html',
   styleUrls: ['./myprofile.component.css']
 })
-export class MyprofileComponent {
+export class MyprofileComponent implements OnInit, OnDestroy {
+  backgroundUrl: string = 'assets/images/fondogris.png';
 
-    backgroundUrl = 'assets/images/fondoLandingMM.png';
+  // Lista de palabras dinámicas
+  words: string[] = ['JUGADOR', 'INNOVADOR', 'APASIONADO', 'CURIOSO'];
+  currentWord: string = this.words[0];
+  private wordIndex: number = 0;
+  private intervalId: any;
 
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.wordIndex = (this.wordIndex + 1) % this.words.length;
+      this.currentWord = this.words[this.wordIndex];
+    }, 3000); // cada 3 segundos
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
