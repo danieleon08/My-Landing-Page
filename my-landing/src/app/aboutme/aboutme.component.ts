2259bd3c9b3f73691147a6127795d7d79668c914
danieleon08 <dflp2005@gmail.com>
2025-12-24 11:44:03 -0500

    agrego contenido

--- my-landing/src/app/aboutme/aboutme.component.ts
+++ my-landing/src/app/aboutme/aboutme.component.ts
@@ -25,16 +25,9 @@ export class AboutmeComponent implements OnInit, AfterViewInit, OnDestroy {
     { icon: 'fas fa-clock', title: 'Gestión del Tiempo', description: 'Capaz de manejar múltiples tareas y cumplir con los plazos' },
     { icon: 'fas fa-paint-brush', title: 'Creatividad', description: 'Aporto ideas innovadoras en proyectos y soluciones' },
     { icon: 'fas fa-user-tie', title: 'Liderazgo', description: 'Experiencia liderando equipos en proyectos universitarios' },
-    { icon: 'fas fa-handshake', title: 'Habilidades Interpersonales', description: 'Capaz de trabajar bien con otros en un entorno de equipo' },
-    { icon: 'fas fa-fire', title: 'Apasionado', description: 'Me apersono mucho de los proyectos o trabajos que me gustan' },
     { icon: 'fas fa-mountain', title: 'Resiliencia', description: 'Capaz de superar desafíos y aprender de los errores' },
-    { icon: 'fas fa-search', title: 'Atención al Detalle', description: 'Me aseguro de que mi trabajo sea preciso y de alta calidad' },
     { icon: 'fas fa-laptop-code', title: 'Habilidades Técnicas', description: 'Conocimiento en lenguajes de programación y herramientas tecnológicas' },
     { icon: 'fas fa-heart', title: 'Empatía', description: 'Capaz de entender y compartir los sentimientos de los demás' },
-    { icon: 'fas fa-tasks', title: 'Organización', description: 'Mantengo mi trabajo y espacio organizado para maximizar la eficiencia' },
-    { icon: 'fas fa-bolt', title: 'Proactividad', description: 'Tomo la iniciativa para mejorar procesos y resolver problemas' },
-    { icon: 'fas fa-chalkboard-teacher', title: 'Habilidades de Presentación', description: 'Capaz de presentar ideas y proyectos de manera efectiva' },
-    { icon: 'fas fa-search-plus', title: 'Curiosidad', description: 'Siempre buscando aprender y descubrir cosas nuevas' },
     { icon: 'fas fa-handshake-angle', title: 'Habilidades de Negociación', description: 'Capaz de llegar a acuerdos beneficiosos en situaciones de conflicto' },
     { icon: 'fas fa-project-diagram', title: 'Pensamiento Estratégico', description: 'Capaz de planificar a largo plazo y anticipar desafíos futuros' }
   ];
--- my-landing/src/app/tech/tech.component.ts
+++ my-landing/src/app/tech/tech.component.ts
@@ -16,7 +16,7 @@ export class TechComponent implements AfterViewInit {
         { name: 'React', icon: 'fab fa-react', color: '#61DAFB' },
         { name: 'TypeScript', icon: 'fab fa-js-square', color: '#3178C6' },
         { name: 'JavaScript', icon: 'fab fa-js', color: '#F7DF1E' },
-        { name: 'Next.js', icon: 'fas fa-code', color: '#000000' }, 
+        { name: 'Next.js', icon: 'fas fa-code', color: '#000000' },
         { name: 'TailwindCSS', icon: 'fas fa-wind', color: '#38B2AC' },
         { name: 'CSS3', icon: 'fab fa-css3-alt', color: '#1572B6' },
         { name: 'HTML5', icon: 'fab fa-html5', color: '#E34F26' },
@@ -33,7 +33,6 @@ export class TechComponent implements AfterViewInit {
         { name: 'C', icon: 'fas fa-copyright', color: '#A8B9CC' },
         { name: 'C++', icon: 'fas fa-code', color: '#00599C' },
         { name: 'Java', icon: 'fab fa-java', color: '#007396' },
-        { name: 'PHP', icon: 'fab fa-php', color: '#777BB4' },
         { name: 'R', icon: 'fas fa-chart-line', color: '#276DC3' },
         { name: 'Prolog', icon: 'fas fa-project-diagram', color: '#E61B23' }
       ]
@@ -44,8 +43,7 @@ export class TechComponent implements AfterViewInit {
         { name: 'Kotlin', icon: 'fas fa-mobile-alt', color: '#7F52FF' },
         { name: 'Flutter', icon: 'fas fa-feather', color: '#02569B' },
         { name: 'Dart', icon: 'fas fa-code', color: '#0175C2' },
-        { name: 'Android Studio', icon: 'fab fa-android', color: '#3DDC84' },
-        { name: 'Swift', icon: 'fab fa-swift', color: '#FA7343' }
+        { name: 'Android Studio', icon: 'fab fa-android', color: '#3DDC84' }
       ]
     },
     {
@@ -55,7 +53,6 @@ export class TechComponent implements AfterViewInit {
         { name: 'PostgreSQL', icon: 'fas fa-database', color: '#336791' },
         { name: 'MySQL', icon: 'fas fa-database', color: '#4479A1' },
         { name: 'Firebase', icon: 'fas fa-fire', color: '#FFCA28' },
-        { name: 'Redis', icon: 'fas fa-cube', color: '#DC382D' },
         { name: 'Oracle', icon: 'fas fa-database', color: '#F80000' },
         { name: 'Supabase', icon: 'fas fa-bolt', color: '#34b27b' }
 
@@ -71,11 +68,9 @@ export class TechComponent implements AfterViewInit {
         { name: 'Azure', icon: 'fas fa-cloud', color: '#0078D7' },
         { name: 'Linux', icon: 'fab fa-linux', color: '#FCC624' },
         { name: 'Bizagi', icon: 'fas fa-project-diagram', color: '#00AEEF' },
-        { name: 'Eclipse', icon: 'fas fa-moon', color: '#2C2255' },
         { name: 'IntelliJ IDEA', icon: 'fas fa-lightbulb', color: '#000000' },
         { name: 'VS Code', icon: 'fas fa-code', color: '#007ACC' },
         { name: 'GNS3', icon: 'fas fa-network-wired', color: '#FF6F00' },
-        { name: 'Packet Tracer', icon: 'fas fa-project-diagram', color: '#00BFFF' },
         { name: 'NetBeans', icon: 'fas fa-leaf', color: '#1B6AC6' },
         { name: 'Visual Paradigm', icon: 'fas fa-sitemap', color: '#E30022' },
         { name: 'VirtualBox', icon: 'fas fa-cube', color: '#183A61' }
@@ -83,7 +78,7 @@ export class TechComponent implements AfterViewInit {
     }
   ];
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) { }
 
   ngAfterViewInit(): void {
     const icons = this.el.nativeElement.querySelectorAll('.tech-icon');
